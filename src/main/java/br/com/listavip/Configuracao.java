af797cffb89b118922f1a979081f942acdf1d057
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2018-05-22 20:12:14 -0300

    criada a entidade Convidado e configuração de persistência

--- src/main/java/br/com/listavip/Configuracao.java
+++ src/main/java/br/com/listavip/Configuracao.java
@@ -1,14 +1,47 @@
 package br.com.listavip;
 
+import javax.sql.DataSource;
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"br.com.listavip.model"})
 public class Configuracao {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Configuracao.class, args);
 	}
 	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/db_listavip");
+		dataSource.setUsername("root");
+		dataSource.setPassword("");
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true); //criação de tabelas automaticamente pelo Hibernate
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL5InnoDBDialect");
+		adapter.setPrepareConnection(true);
+		
+		return adapter;
+	}
+
 	
 }
